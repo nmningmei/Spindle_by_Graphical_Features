3702fe39249056e2e7cc051dc957ade98d0804a4
Ning Mei <semsociaty@gmail.com>
2017-05-22 19:22:26 -0400

    change subfolder logic

--- train on extracted features.py
+++ train on extracted features.py
@@ -8,60 +8,27 @@ Created on Sun May 21 13:13:26 2017
 import pandas as pd
 import os
 import numpy as np
-os.chdir('D:\\NING - spindle\\Spindle_by_Graphical_Features')
+try:
+    function_dir = 'D:\\NING - spindle\\Spindle_by_Graphical_Features'
+    os.chdir(function_dir)
+except:
+    function_dir = 'C:\\Users\\ning\\OneDrive\\python works\\Spindle_by_Graphical_Features'
+    os.chdir(function_dir)
 import eegPipelineFunctions
-raw_dir = 'D:\\NING - spindle\\training set\\'
-# get EEG files that have corresponding annotations
-raw_files = []
-for file in [f for f in os.listdir(raw_dir) if ('txt' in f)]:
-    sub = int(file.split('_')[0][3:])
-    if sub < 11:
-        day = file.split('_')[1][1]
-        day_for_load = file.split('_')[1][:2]
-    else:
-        day = file.split('_')[2][-1]
-        day_for_load = file.split('_')[2]
-    raw_file = [f for f in os.listdir(raw_dir) if (file.split('_')[0] in f) and (day_for_load in f) and ('fif' in f)]
-    if len(raw_file) != 0:
-        raw_files.append([raw_dir + raw_file[0],raw_dir + file])
-# directory for storing all the feature files
-raw_dir = 'D:\\NING - spindle\\training set\\road_trip\\'
-# initialize the range of the parameters we want to compute based on
-epoch_lengths  = np.arange(1.5,5.5,0.5) # 1.5 to 5 seconds with 0.5 stepsize
-plv_thresholds = np.arange(0.6, 0.85, 0.05) # 0.6 to 0.8 with .05
-pli_thresholds = np.arange(0.05,0.30, 0.05) # 0.05 to 0.25 with 0.05
-cc_thresholds  = np.arange(0.7, 0.95,0.05) # 0.7 to 0.9 with 0.05
-# make sub-directories based on epoch length
-first_level_directory = []
-signal_feature_dict = {str(epoch_length):[] for epoch_length in epoch_lengths}
-for epoch_length in epoch_lengths:
-    directory_1 = raw_dir + 'epoch_length '+str(epoch_length)+'\\'
-    if not os.path.exists(directory_1):
-        os.makedirs(directory_1)
-    first_level_directory.append(directory_1)   
-    df_cc,df_pli,df_plv,df_signal=[],[],[],[]
+try:
+    file_dir = 'D:\\NING - spindle\\training set\\road_trip\\'
+    os.chdir(file_dir)
+except:
+    file_dir = 'C:\\Users\\ning\\Downloads\\road_trip\\'
+    os.chdir(file_dir)
     
-    os.chdir(directory_1)
-    #print(os.getcwd())
-    for files in raw_files:
-        raw_file, annotation_file = files
-        temp_anno = annotation_file.split('\\')[-1]
-        sub = int(temp_anno.split('_')[0][3:])
-        if sub < 11:
-            day = temp_anno.split('_')[1][1]
-            day_for_load = temp_anno.split('_')[1][:2]
-        else:
-            day = temp_anno.split('_')[2][-1]
-            day_for_load = temp_anno.split('_')[2]
-        directory_2 = directory_1 + 'sub' + str(sub) + 'day' + day + '\\'
-        if not os.path.exists(directory_2):
-            #print(directory_2)
-            os.makedirs(directory_2)
-        os.chdir(directory_2)
-        cc_feature, pli_feature, plv_feature, signal_feature=[pd.read_csv(f) for f in os.listdir(directory_2) if ('csv' in f)]
-        df_cc.append(cc_feature)
-        df_pli.append(pli_feature)
-        df_plv.append(plv_feature)
-        df_signal.append(signal_feature)
-    signal_feature_dict[str(epoch_length)] = pd.concat(df_signal)
+for directory_1 in os.listdir(file_dir):
+    sub_dir = file_dir + directory_1 + '\\'
+    os.chdir(sub_dir)
+    for sub_fold in os.listdir(sub_dir):
+        sub_fold_dir = sub_dir + sub_fold + '\\'
+        os.chdir(sub_fold_dir)
+        print(os.listdir(os.getcwd()))
+    
+
         
