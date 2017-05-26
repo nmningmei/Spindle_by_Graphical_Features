fc527e8dd83675e78fa706b1e2bec38efd199094
Ning Mei <semsociaty@gmail.com>
2017-05-26 18:53:35 -0400

    update

--- individual performance.py
+++ individual performance.py
@@ -31,13 +31,13 @@ except:
     os.chdir(file_dir)
 #siganl_only_result = pickle.load(open(file_dir+'signal_feature_only.p','rb'))
 #graph_only_result = pickle.load(open(file_dir+'graph_feature_only.p','rb'))
-signal_features_dict = {}
-graph_features_dict = {}
+signal_features_indivisual_results = {}
+graph_features_indivisual_results = {}
 for directory_1 in [f for f in os.listdir(file_dir) if ('epoch_length' in f)]:
     sub_dir = file_dir + directory_1 + '\\'
     epoch_length = directory_1[-3]
     os.chdir(sub_dir)
-    signal_features_dict[directory_1],graph_features_dict[directory_1]={},{}
+    signal_features_indivisual_results[directory_1],graph_features_indivisual_results[directory_1]={},{}
     #df_cc, df_pli, df_plv, df_signal,df_graph = [],[],[],[],[]
     for sub_fold in os.listdir(sub_dir):
         sub_fold_dir = sub_dir + sub_fold + '\\'
@@ -61,19 +61,19 @@ for directory_1 in [f for f in os.listdir(file_dir) if ('epoch_length' in f)]:
         df_combine = pd.concat([cc_features,pli_features,plv_features],axis=1)
         df_combine['label']=label
         try:
-            signal_features_dict[directory_1][sub_fold] = eegPipelineFunctions.cross_validation_pipeline(signal_features)
-            graph_features_dict[directory_1][sub_fold] = eegPipelineFunctions.cross_validation_pipeline(df_combine)
+            signal_features_indivisual_results[directory_1][sub_fold] = eegPipelineFunctions.cross_validation_pipeline(signal_features)
+            graph_features_indivisual_results[directory_1][sub_fold] = eegPipelineFunctions.cross_validation_pipeline(df_combine)
             print(sub_fold,Counter(label),'enough samples??')
-            print('signal:%.2f +/-%.2f'%(np.mean([v for (v,_,_,_) in signal_features_dict[directory_1][sub_fold]]),np.std([v for (v,_,_,_) in signal_features_dict[directory_1][sub_fold]])))
-            print('graph:%.2f +/-%.2f'%(np.mean([v for (v,_,_,_) in graph_features_dict[directory_1][sub_fold]]),np.std([v for (v,_,_,_) in graph_features_dict[directory_1][sub_fold]])))
+            print('signal:%.2f +/-%.2f'%(np.mean([v for (v,_,_,_) in signal_features_indivisual_results[directory_1][sub_fold]]),np.std([v for (v,_,_,_) in signal_features_indivisual_results[directory_1][sub_fold]])))
+            print('graph:%.2f +/-%.2f'%(np.mean([v for (v,_,_,_) in graph_features_indivisual_results[directory_1][sub_fold]]),np.std([v for (v,_,_,_) in graph_features_indivisual_results[directory_1][sub_fold]])))
             #print(confusion_matrix())
         except:
             try:
                 cv = KFold(n_splits=5,shuffle=True,random_state=0)
-                signal_features_dict[directory_1][sub_fold] = eegPipelineFunctions.cross_validation_pipeline(signal_features,cv=cv)
-                graph_features_dict[directory_1][sub_fold] = eegPipelineFunctions.cross_validation_pipeline(df_combine,cv=cv)
+                signal_features_indivisual_results[directory_1][sub_fold] = eegPipelineFunctions.cross_validation_pipeline(signal_features,cv=cv)
+                graph_features_indivisual_results[directory_1][sub_fold] = eegPipelineFunctions.cross_validation_pipeline(df_combine,cv=cv)
                 print(sub_fold,Counter(label),'too few samples??')
             except:
                 print(sub_fold,Counter(label),'not enough samples')
-pickle.dump(signal_features_dict,open(file_dir+'individual_signal_feature_only.p','wb'))
-pickle.dump(graph_features_dict,open(file_dir+'individual_graph_feature_only.p','wb'))
+pickle.dump(signal_features_indivisual_results,open(file_dir+'individual_signal_feature_only.p','wb'))
+pickle.dump(graph_features_indivisual_results,open(file_dir+'individual_graph_feature_only.p','wb'))
--- train on extracted features.py
+++ train on extracted features.py
@@ -13,6 +13,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegressionCV,LogisticRegression
 from sklearn.metrics import roc_curve,precision_recall_curve,auc,average_precision_score,confusion_matrix,classification_report
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
 import pickle
 try:
     function_dir = 'D:\\NING - spindle\\Spindle_by_Graphical_Features'
@@ -28,86 +30,58 @@ except:
     file_dir = 'C:\\Users\\ning\\Downloads\\road_trip\\'
     os.chdir(file_dir)
 
-signal_features_dict = {}
-graph_features_dict = {}
-for directory_1 in [f for f in os.listdir(file_dir) if ('epoch_length' in f)]:
-    sub_dir = file_dir + directory_1 + '\\'
-    epoch_length = directory_1[-3]
-    os.chdir(sub_dir)
-    df_cc, df_pli, df_plv, df_signal,df_graph = [],[],[],[],[]
-    for sub_fold in os.listdir(sub_dir):
-        sub_fold_dir = sub_dir + sub_fold + '\\'
-        os.chdir(sub_fold_dir)
-        cc_features, pli_features, plv_features, signal_features = [pd.read_csv(f) for f in os.listdir(sub_fold_dir) if ('csv' in f)]
-        #df_cc.append(cc_features)
-        #df_pli.append(pli_features)
-        #df_plv.append(plv_features)
-        label = cc_features['label']
-        cc_features = eegPipelineFunctions.get_real_part(cc_features)
-        pli_features = eegPipelineFunctions.get_real_part(pli_features)
-        plv_features = eegPipelineFunctions.get_real_part(plv_features)
-        cc_features.columns = ['cc_'+name for name in cc_features]
-        pli_features.columns = ['pli_'+name for name in pli_features]
-        plv_features.columns = ['plv_'+name for name in plv_features]
-        cc_features = cc_features.drop('cc_label',1)
-        pli_features = pli_features.drop('pli_label',1)
-        plv_features = plv_features.drop('plv_label',1)
-        df_combine = pd.concat([cc_features,pli_features,plv_features],axis=1)
-        df_combine['label']=label
-        df_signal.append(signal_features)
-        df_graph.append(df_combine)
-    signal_features_dict[directory_1] = pd.concat(df_signal)
-    graph_features_dict[directory_1]  = pd.concat(df_graph)
+if False:
+    signal_features_dict = {}
+    graph_features_dict = {}
+    for directory_1 in [f for f in os.listdir(file_dir) if ('epoch_length' in f)]:
+        sub_dir = file_dir + directory_1 + '\\'
+        epoch_length = directory_1[-3]
+        os.chdir(sub_dir)
+        df_cc, df_pli, df_plv, df_signal,df_graph = [],[],[],[],[]
+        for sub_fold in os.listdir(sub_dir):
+            sub_fold_dir = sub_dir + sub_fold + '\\'
+            os.chdir(sub_fold_dir)
+            cc_features, pli_features, plv_features, signal_features = [pd.read_csv(f) for f in os.listdir(sub_fold_dir) if ('csv' in f)]
+            #df_cc.append(cc_features)
+            #df_pli.append(pli_features)
+            #df_plv.append(plv_features)
+            label = cc_features['label']
+            cc_features = eegPipelineFunctions.get_real_part(cc_features)
+            pli_features = eegPipelineFunctions.get_real_part(pli_features)
+            plv_features = eegPipelineFunctions.get_real_part(plv_features)
+            cc_features.columns = ['cc_'+name for name in cc_features]
+            pli_features.columns = ['pli_'+name for name in pli_features]
+            plv_features.columns = ['plv_'+name for name in plv_features]
+            cc_features = cc_features.drop('cc_label',1)
+            pli_features = pli_features.drop('pli_label',1)
+            plv_features = plv_features.drop('plv_label',1)
+            df_combine = pd.concat([cc_features,pli_features,plv_features],axis=1)
+            df_combine['label']=label
+            df_signal.append(signal_features)
+            df_graph.append(df_combine)
+        signal_features_dict[directory_1] = pd.concat(df_signal)
+        graph_features_dict[directory_1]  = pd.concat(df_graph)
+        pickle.dump(signal_features_dict,open(file_dir+'signal_features_data.p','wb'))
+        pickle.dump(graph_features_dict, open(file_dir+'graph_features_data.p','wb'))
+else:
+    signal_features_dict=pickle.load(open(file_dir+'signal_features_data.p','rb'))
+    graph_features_dict= pickle.load(open(file_dir+'graph_features_data.p','rb'))
 
-results = {}
-for key,dfs in signal_features_dict.items():
-    data = dfs.values   
-    X, Y = data[:,:-1], data[:,-1]
-    cv = StratifiedKFold(n_splits=10,shuffle=True,random_state=np.random.randint(10000,20000))
-    results[key] = []
-    for train, test in cv.split(X,Y):
-        clf = Pipeline([('scaler',StandardScaler()),
-                        ('estimator',LogisticRegressionCV(Cs=np.logspace(-3,3,7),
-                                                          max_iter=int(1e5),
-                                                          tol=1e-4,
-                                                          cv=KFold(n_splits=10,shuffle=True,random_state=12345),
-                                                          class_weight={1:1-np.count_nonzero(Y)/len(Y),0:(np.count_nonzero(Y)/len(Y))},
-                                                          scoring='roc_auc'))])
-        clf.fit(X[train],Y[train])
-        fpr,tpr,_ = roc_curve(Y[test],clf.predict_proba(X[test])[:,-1])
-        auc_score = auc(fpr,tpr)
-        precision,recall,_ = precision_recall_curve(Y[test],clf.predict_proba(X[test])[:,-1])
-        average_scores = average_precision_score(Y[test],clf.predict_proba(X[test])[:,-1])
-        results[key].append([auc_score,fpr,tpr,precision,recall,average_scores])
-        print(key,auc_score,precision,recall,average_scores,'signal\n',confusion_matrix(Y[test],clf.predict(X[test])),'\n',
-              classification_report(Y[test],clf.predict(X[test])))
-pickle.dump(results,open(file_dir+'signal_feature_only.p','wb'))
+##### logitsic regression models ##########
+plt.close('all')
+keys=['epoch_length 1.5', 'epoch_length 2.0', 
+      'epoch_length 2.5', 'epoch_length 3.0', 
+      'epoch_length 3.5', 'epoch_length 4.0', 
+      'epoch_length 4.5', 'epoch_length 5.0']
+"""
+fig=eegPipelineFunctions.visualize_auc_precision_recall(signal_features_dict,keys,subtitle='Signal features only, logistic regression')
+fig.tight_layout(pad=3.5)
+fig.savefig(file_dir+'results\\signal_feature_sum_results.png')
 
-results = {}
-for key,dfs in graph_features_dict.items():
-    data = dfs.values   
-    #dfs.to_csv(file_dir+'%s.csv'%key)
-    X, Y = data[:,:-1], data[:,-1]
-    cv = StratifiedKFold(n_splits=20,shuffle=True,random_state=np.random.randint(10000,20000))
-    results[key] = []
-    for train, test in cv.split(X,Y):
-        clf = Pipeline([('scaler',StandardScaler()),
-                        ('estimator',LogisticRegressionCV(Cs=np.logspace(-3,3,7),
-                                                          max_iter=int(1e5),
-                                                          tol=1e-4,
-                                                          cv=KFold(n_splits=10,shuffle=True,random_state=12345),
-                                                          class_weight={1:1-np.count_nonzero(Y)/len(Y),0:(np.count_nonzero(Y)/len(Y))},
-                                                          scoring='roc_auc'))])
-        #clf = LogisticRegression(C=1e-8,max_iter=int(1e5),tol=1e-4)
-        clf.fit(X[train],Y[train])
-        fpr,tpr,_ = roc_curve(Y[test],clf.predict_proba(X[test])[:,-1])
-        auc_score = auc(fpr,tpr)
-        precision,recall,_ = precision_recall_curve(Y[test],clf.decision_function(X[test]))
-        average_scores = average_precision_score(Y[test],clf.decision_function(X[test]))
-        results[key].append([auc_score,fpr,tpr,precision,recall,average_scores])
-        print(key,auc_score,precision,recall,average_scores,'graph\n',confusion_matrix(Y[test],clf.predict(X[test])),'\n',
-              classification_report(Y[test],clf.predict(X[test])))
-pickle.dump(results,open(file_dir+'graph_feature_only.p','wb'))         
-    
-#siganl_only_result = pickle.load(open(file_dir+'signal_feature_only.p','rb'))
-#graph_only_result = pickle.load(open(file_dir+'graph_feature_only.p','rb'))
+fig=eegPipelineFunctions.visualize_auc_precision_recall(graph_features_dict,keys,subtitle='Graph features only, logistic regression')
+fig.tight_layout(pad=3.5)
+fig.savefig(file_dir+'results\\graph_feature_sum_results.png')
+"""
+####### svm models  rbf kernel ################
+fig=eegPipelineFunctions.visualize_auc_precision_recall(signal_features_dict,keys,clf_='svm',kernel='rbf')
+
