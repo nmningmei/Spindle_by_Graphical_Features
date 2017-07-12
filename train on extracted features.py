b05bbf13a4dd166bed90cf789408ad3a3d3c2d62
Ning Mei <semsociaty@gmail.com>
2017-07-12 14:43:41 -0400

    update
    1. turn back to 6 channels

--- train on extracted features.py
+++ train on extracted features.py
@@ -18,15 +18,15 @@ except:
     os.chdir(function_dir)
 import eegPipelineFunctions
 try:
-#    file_dir = 'D:\\NING - spindle\\training set\\road_trip\\'
-    file_dir = 'D:\\NING - spindle\\training set\\road_trip_more_channels\\'
+    file_dir = 'D:\\NING - spindle\\training set\\road_trip\\'
+#    file_dir = 'D:\\NING - spindle\\training set\\road_trip_more_channels\\'
     os.chdir(file_dir)
 except:
-#    file_dir = 'C:\\Users\\ning\\Downloads\\road_trip\\'
-    file_dir = 'C:\\Users\\ning\\Downloads\\road_trip_more_channels\\'
+    file_dir = 'C:\\Users\\ning\\Downloads\\road_trip\\'
+#    file_dir = 'C:\\Users\\ning\\Downloads\\road_trip_more_channels\\'
     os.chdir(file_dir)
 
-if False:
+if True:
     signal_features_dict = {}
     graph_features_dict = {}
     for directory_1 in [f for f in os.listdir(file_dir) if ('epoch_length' in f)]:
@@ -62,14 +62,10 @@ if False:
 else:
     signal_features_dict=pickle.load(open(file_dir+'signal_features_data.p','rb'))
     graph_features_dict= pickle.load(open(file_dir+'graph_features_data.p','rb'))
-
+keys=['epoch_length ' + str(a) for a in np.arange(1.,5.,0.2)]
 ##### logitsic regression models ##########
 plt.close('all')
-keys=['epoch_length 1.5', 'epoch_length 2.0', 
-      'epoch_length 2.5', 'epoch_length 3.0', 
-      'epoch_length 3.5', 'epoch_length 4.0', 
-      'epoch_length 4.5', 'epoch_length 5.0',
-      'epoch_length 5.5']
+
 
 fig=eegPipelineFunctions.visualize_auc_precision_recall(signal_features_dict,keys,clf_='logistic',subtitle='Signal features only, logistic regression')
 fig.tight_layout(pad=3.5)
