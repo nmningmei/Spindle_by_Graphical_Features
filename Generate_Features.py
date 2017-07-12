e67659ba53f1227e03533e92c45d3c0bc7fc86e3
Ning Mei <semsociaty@gmail.com>
2017-07-12 14:43:20 -0400

    re-run
    1. smaller step size for epoch length
    2. no thresholding for the connnectivity measure
    3. rerun the script

--- Generate_Features.py
+++ Generate_Features.py
@@ -5,11 +5,12 @@ Created on Wed May 17 12:35:28 2017
 @author: ning
 """
 
-import mne
+#import mne
 import numpy as np
 import pandas as pd
 import os
-import networkx as nx
+#import networkx as nx
+from collections import Counter
 os.chdir('D:\\NING - spindle\\Spindle_by_Graphical_Features')
 channelList = ['F3','F4','C3','C4','O1','O2']
 import eegPipelineFunctions
@@ -34,7 +35,7 @@ raw_dir = 'D:\\NING - spindle\\training set\\road_trip\\'
 if not os.path.exists(raw_dir):
     os.makedirs(raw_dir)
 # initialize the range of the parameters we want to compute based on
-epoch_lengths  = np.arange(1.5,5.5,0.5) # 1.5 to 5 seconds with 0.5 stepsize
+epoch_lengths  = np.arange(1.,5.,0.2) # 1. to 5 seconds with 0.5 stepsize
 plv_thresholds = np.arange(0.6, 0.85, 0.05) # 0.6 to 0.8 with .05
 pli_thresholds = np.arange(0.05,0.30, 0.05) # 0.05 to 0.25 with 0.05
 cc_thresholds  = np.arange(0.7, 0.95,0.05) # 0.7 to 0.9 with 0.05
@@ -67,6 +68,7 @@ for epoch_length in epoch_lengths:
         epochs,label,_ = eegPipelineFunctions.get_data_ready(raw_file,channelList,
                                                              annotation_file,
                                                              epoch_length=epoch_length)
+        print(Counter(label))
         # extract signal features
         epochFeature = eegPipelineFunctions.featureExtraction(epochs,)
         epochFeature = pd.DataFrame(epochFeature)
@@ -87,31 +89,31 @@ for epoch_length in epoch_lengths:
         pli_pre_threshold.to_csv('sub'+str(sub)+'day'+day+'pli_features.csv',index=False)
         cc_pre_threshold.to_csv('sub'+str(sub)+'day'+day+'cc_features.csv',index=False)
         print('done plv, pli, and cc')
-        print('start thresholding')
-        # extract graph features
-        for t_plv,t_pli,t_cc in zip(plv_thresholds,pli_thresholds,cc_thresholds):
-            # convert adjasency matrices to binary adjasency matrices
-            adj_plv = eegPipelineFunctions.thresholding(t_plv,plv)
-            adj_pli = eegPipelineFunctions.thresholding(t_pli,pli)
-            adj_cc  = eegPipelineFunctions.thresholding(t_cc, cc )
-            # this is how we extract graph features
-            graphFeature_plv = eegPipelineFunctions.extractGraphFeatures(adj_plv)
-            graphFeature_pli = eegPipelineFunctions.extractGraphFeatures(adj_pli)
-            graphFeature_cc  = eegPipelineFunctions.extractGraphFeatures(adj_cc )
-            # prepare the sub-directories for storing feature files
-            plv_dir = directory_2 + 'plv_' + str(t_plv) + '\\'
-            pli_dir = directory_2 + 'pli_' + str(t_pli) + '\\'
-            cc_dir  = directory_2 + 'cc_'  + str(t_cc ) + '\\'
-            if not os.path.exists(plv_dir):
-                os.makedirs(plv_dir)
-            if not os.path.exists(pli_dir):
-                os.makedirs(pli_dir)
-            if not os.path.exists(cc_dir):
-                os.makedirs(cc_dir)
-            # saving csvs
-            pd.concat([epochFeature,graphFeature_plv],axis=1).to_csv(plv_dir + 'plv_' + str(t_plv) + '.csv',index=False)
-            pd.concat([epochFeature,graphFeature_pli],axis=1).to_csv(pli_dir + 'pli_' + str(t_pli) + '.csv',index=False)
-            pd.concat([epochFeature,graphFeature_cc ],axis=1).to_csv(cc_dir  + 'cc_'  + str(t_cc ) + '.csv',index=False)
+#        print('start thresholding')
+#        # extract graph features
+#        for t_plv,t_pli,t_cc in zip(plv_thresholds,pli_thresholds,cc_thresholds):
+#            # convert adjasency matrices to binary adjasency matrices
+#            adj_plv = eegPipelineFunctions.thresholding(t_plv,plv)
+#            adj_pli = eegPipelineFunctions.thresholding(t_pli,pli)
+#            adj_cc  = eegPipelineFunctions.thresholding(t_cc, cc )
+#            # this is how we extract graph features
+#            graphFeature_plv = eegPipelineFunctions.extractGraphFeatures(adj_plv)
+#            graphFeature_pli = eegPipelineFunctions.extractGraphFeatures(adj_pli)
+#            graphFeature_cc  = eegPipelineFunctions.extractGraphFeatures(adj_cc )
+#            # prepare the sub-directories for storing feature files
+#            plv_dir = directory_2 + 'plv_' + str(t_plv) + '\\'
+#            pli_dir = directory_2 + 'pli_' + str(t_pli) + '\\'
+#            cc_dir  = directory_2 + 'cc_'  + str(t_cc ) + '\\'
+#            if not os.path.exists(plv_dir):
+#                os.makedirs(plv_dir)
+#            if not os.path.exists(pli_dir):
+#                os.makedirs(pli_dir)
+#            if not os.path.exists(cc_dir):
+#                os.makedirs(cc_dir)
+#            # saving csvs
+#            pd.concat([epochFeature,graphFeature_plv],axis=1).to_csv(plv_dir + 'plv_' + str(t_plv) + '.csv',index=False)
+#            pd.concat([epochFeature,graphFeature_pli],axis=1).to_csv(pli_dir + 'pli_' + str(t_pli) + '.csv',index=False)
+#            pd.concat([epochFeature,graphFeature_cc ],axis=1).to_csv(cc_dir  + 'cc_'  + str(t_cc ) + '.csv',index=False)
 
 
  
